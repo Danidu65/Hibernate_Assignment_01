cd8ccfe7e858111716b559c64b06c63c74c4c2eb
Danidu Viraj <daniduijse65@Gmail.com>
2023-08-04 01:06:27 +0530

    Changes

--- Hibernate-ORM-Assignment-I-main/part 2/many-to-many-relationship/src/main/java/FactoryConfig.java
+++ Hibernate-ORM-Assignment-I-main/part 2/many-to-many-relationship/src/main/java/FactoryConfig.java
@@ -3,7 +3,6 @@ import entity.Book;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 public class FactoryConfig {
     private static FactoryConfig factoryConfig;
     private SessionFactory sessionFactory;
